1b02c4a614afd6a038deb11e68d52daf166955c5
Gokulraj0606 <gokul222.gr@gmail.com>
2024-02-27 14:22:28 +0530

    initial

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET: "xTfp20LV8hVDoiJ3xfmyH6DHg2oYhBJMaAsw8z23//4=",
+    EMAIL: "osvaldo7@ethereal.email",
+    PASSWORD: "T3zjQKv1WEeZ2tBg25",
+    ATLAS_URI: "mongodb+srv://admin:admin123@cluster0.rlnb7d8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+}
